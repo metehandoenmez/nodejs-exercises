4e9f651b343cdf924959dca85d615992d0c5e92e
metehandoenmez <metehandoenmez@gmail.com>
2023-07-18 17:22:26 +0300

    completed exercise 10

--- readme.md
+++ readme.md
@@ -8,35 +8,23 @@ You can use **npm install** to install the dependencies and **npm run dev** to t
 
 # Do
 
-> Write simple Express server that listens on port 3000 (use dotenv to specify the port)
-> Create a dummy "database" of planets using a let variable. (You will use this data in further exercises.)
-> Configure your app (app.use()) to:
->
-> > accept JSON from the Client
-> > log the Client's requests
+> Write a router with the following routes:
+
+1. **GET /api/planets:** return all planets (JSON) with **200**
+2. **GET /api/planets/:id:** return a planet (JSON) by id with **200**
+3. **POST /api/planets:** create a planet, return only **201** code and a success JSON with key **msg**.
+4. 1. Make sure every planet is created with **id** and **name**.
+5. **PUT /api/planets/:id:** update a planet by id, return only **200** code and a success JSON with key msg
+6. **DELETE /api/planets/:id:** delete a planet by id, return only **200** code and a success JSON with key **msg**.
+
+   > Validate planet fields where appropriate.
 
 # Use
 
-> Dummy database with initial data:
+1. The dummy database of planets from the previous exercise.
+2. **joi** library for validation.
 
-```
-type Planet = {
-id: number,
-name: string,
-};
+# Check
 
-type Planets = Planet[];
-
-let planets: Planets = [
-{
-id: 1,
-name: "Earth",
-},
-{
-id: 2,
-name: "Mars",
-},
-];
-express-async-errors
-morgan
-```
+1. Use Postman to test the routes.
+2. Paths **POST** and **PUT** should receive data in JSON format (**req.body**).
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import "express-async-errors";
 import morgan from "morgan";
+import { Request, Response } from "express";
 
 import dotenv from "dotenv";
+import Joi from "joi";
+import { request } from "http";
 dotenv.config();
 
 const app = express();
@@ -31,8 +34,61 @@ let planets: Planets = [
   },
 ];
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello" });
+app.get("/api/planets", (req, res) => {
+  res.status(200).json(planets);
+  console.log(
+    "Planet Names:",
+    planets.map((planet) => planet.name)
+  );
+});
+
+app.get("/api/planets/:id", (req: Request, res: Response) => {
+  const { id } = req.params;
+  let planet = planets.find((p) => p.id === Number(id));
+  res.status(200).json(planet);
+  console.log("Planet Name:", planet);
+});
+
+const planetSchema = Joi.object({
+  id: Joi.number().integer().required(),
+  name: Joi.string().required(),
+});
+
+app.post("/api/planets", (req: Request, res: Response) => {
+  const { id, name } = req.body;
+  const newPlanet: Planet = { id, name };
+
+  const validatedNewPlanet = planetSchema.validate(newPlanet);
+
+  if (validatedNewPlanet.error) {
+    res.status(400);
+    res.json({ msg: validatedNewPlanet.error });
+  } else {
+    planets = [...planets, newPlanet];
+    res.status(201);
+    res.json({ msg: "The planet was created." });
+  }
+
+  console.log(planets);
+});
+
+app.put("/api/planets/:id", (req: Request, res: Response) => {
+  let { id } = req.params;
+  let { name } = req.body;
+
+  planets = planets.map((p) => (p.id === Number(id) ? { ...p, name } : p));
+
+  res.status(200).json({ msg: "Success! The planet is updated." });
+
+  console.log(planets);
+});
+
+app.delete("/api/planets/:id", (req: Request, res: Response) => {
+  let { id } = req.params;
+  planets = planets.filter((p) => p.id !== Number(id));
+
+  res.status(200).json({ msg: "Success! The planet is deleted." });
+  console.log(planets);
 });
 
 app.listen(port, () => {
